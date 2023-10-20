5c9255869753bae172476a5167a8674bcb7129ea
Toastie <toastie@toastielab.xyz>
2023-10-19 22:29:51 -0700

    Update src/components/Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -41,7 +41,7 @@ export const Hero = () => {
           transition={{ duration: 0.5, delay: 0.1 }}
         >
           <div className="text-customGrayText text-sm lg:text-base xl:text-lg sm:text-base mt-10 px-12 sm:px-48 ">
-            we are a company that likes to have fun
+            We are a company that likes to have fun
           </div>
         </motion.div>
         <motion.div
