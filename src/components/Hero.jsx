268a1c4c24fa84aaedc7dabd5d6ad55df9c88105
Toastie <toastie@toastielab.xyz>
2023-10-19 22:23:26 -0700

    Update src/components/Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,8 +28,8 @@ export const Hero = () => {
           transition={{ duration: 0.5, delay: 0.05 }}
         >
           <div className="text-5xl sm:text-6xl lg:text-7xl xl:text-7xl font-bold tracking-wide  text-white  px-8 sm:px-8 md:px-20 lg:px-4">
-            <span className="inline md:hidden">The Software & Hosting</span>{" "}
-            <span className="hidden md:inline">The Software & Hosting</span>
+            <span className="inline md:hidden">The Hosting</span>{" "}
+            <span className="hidden md:inline">The Hosting</span>
           </div>
           <div className="mt-2 sm:mt-2 text-4xl sm:text-6xl lg:text-7xl xl:text-7xl font-bold tracking-wide  text-white  px-8 sm:px-20 md:px-24 lg:px-24">
             Company
