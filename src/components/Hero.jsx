1c75dd6a6b08449aeffbe5c3d995644b7f821282
Toastie <toastie@toastielab.xyz>
2023-10-20 00:18:53 -0700

    Update src/components/Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ export const Hero = () => {
           transition={{ duration: 0.5 }}
         >
           <div className="text-customSecondary text-sm sm:text-base  mb-6 sm:mt-32 mt-16  font-bold">
-            Dragons child
+            Dragons child studios
           </div>
         </motion.div>
         <motion.div
