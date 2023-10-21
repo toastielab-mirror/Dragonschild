d687d7b2a955ce115a6b10a48a7aee91af326525
Emotion <emotion@emotionchild.com>
2023-10-21 21:00:19 +1300

    Added a sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import sitemap from "@astrojs/sitemap";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  site: 'https://dragonschildstudios.com',
+  integrations: [react(), tailwind(), sitemap()]
 });
