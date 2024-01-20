c3200077d639d8375358ec4a0e3e41571795819e
Toastie <toastie@toastiet0ast.com>
2024-01-20 20:23:29 +1300

    Major site update

---
+++ src/navigation.js
@@ -0,0 +1,83 @@
+import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
+
+export const headerData = {
+  links: [
+    {
+      text: 'Pages',
+      links: [
+        {
+          text: 'About us',
+          href: getPermalink('/about'),
+        },
+        {
+          text: 'Contact',
+          href: getPermalink('/contact'),
+        },
+        {
+          text: 'Terms',
+          href: getPermalink('/terms'),
+        },
+      ],
+    },
+    {
+      text: 'Blog',
+      links: [
+        {
+          text: 'Blog List',
+          href: getBlogPermalink(),
+        },
+        {
+          text: 'Article',
+          href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
+        },
+        {
+          text: 'Article (with MDX)',
+          href: getPermalink('markdown-elements-demo-post', 'post'),
+        },
+        {
+          text: 'Category Page',
+          href: getPermalink('tutorials', 'category'),
+        },
+        {
+          text: 'Tag Page',
+          href: getPermalink('astro', 'tag'),
+        },
+      ],
+    },
+  ],
+};
+
+export const footerData = {
+  links: [
+    {
+      title: 'Our projects',
+      links: [
+        { text: 'Toastielab', href: 'https://toastielab.dev' },
+        { text: 'Dragons child hosting', href: 'https://dragonschildhosting.net' },
+        { text: 'Valkyriecoms', href: 'https://www.valkyriecoms.com' },
+      ],
+    },
+    {
+      title: 'Support',
+      links: [
+        { text: 'Status', href: 'https://status.dragonschildhosting.net' },
+      ],
+    },
+    {
+      title: 'Company',
+      links: [
+        { text: 'About', href: '/about' },
+      ],
+    },
+  ],
+  secondaryLinks: [
+    { text: 'Terms', href: getPermalink('/terms') },
+  ],
+  socialLinks: [
+    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
+    { ariaLabel: 'Toastielab', icon: 'tabler:brand-git', href: 'https://toastielab.dev/Dragonschildstudios/dragonschildstudios.com' },
+  ],
+  footNote: `
+    <a class="text-blue-600 hover:underline dark:text-gray-200" href="https://dragonschildstudios.com/"> Dragons child studios</a> · All rights reserved.
+  `,
+};
